2ea610b328deb0ce3cfad048316c8d1058ae3fae
m.sobczyk.official@gmail.com <m.sobczyk.official@gmail.com>
2023-03-03 12:50:39 +0100

    added Move.cs

---
+++ Chess/Move.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Move
+    {
+        public void FindPawnMoves() { }
+        public void FindRookMoves() { }
+        public void FindKnightMoves() { }
+        public void FindBishopMoves() { }
+        public void FindKingMoves() { }
+        public void FindQueenMoves() { }
+
+
+    }
+}
